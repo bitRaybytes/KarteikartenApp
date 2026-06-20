7251e66d322d5c80783e457ad8cf61faa9de0dc4
bitRaybytes <173811320+bitRaybytes@users.noreply.github.com>
2026-06-20 17:45:23 +0200

    Added starting point

--- application/src/Main.java
+++ application/src/Main.java
@@ -1,10 +1,11 @@
+import controller.FlashcardsController;
 import view.MainFrame;
 
 
 public class Main {
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(()->{
-
+            new FlashcardsController().start();
         });
     }
 }
